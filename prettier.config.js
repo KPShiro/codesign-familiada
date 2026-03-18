6432acd08382ce930d708f0e1691993a68fa7def
Kacper Popczynski <popkacper@gmail.com>
2026-03-18 18:25:31 +0100

    feat: add Prettier configuration and update dependencies

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+export default {
+  plugins: ['prettier-plugin-tailwindcss'],
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+}
